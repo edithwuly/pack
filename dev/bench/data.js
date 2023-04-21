e9b63cce175c8546d8949c165460c778c6f35a0f
github-action-benchmark <github@users.noreply.github.com>
2023-04-21 08:46:53 +0000

    add Go Benchmark (go) benchmark result for 4b2ebaaaf21b43192238149c83652783a3fe0978

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682066811375,
+  "lastUpdate": 1682066813432,
   "repoUrl": "https://github.com/edithwuly/pack",
   "entries": {
     "Go Benchmark": [
@@ -95,6 +95,48 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/edithwuly/pack/commit/4b2ebaaaf21b43192238149c83652783a3fe0978"
         },
         "date": 1682066810353,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkBuild/with_Untrusted_Builder",
+            "value": 8330259789,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Trusted_Builder",
+            "value": 2454954486,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Addtional_Buildpack",
+            "value": 36320167862,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "edithwuly@163.com",
+            "name": "edithwuly",
+            "username": "edithwuly"
+          },
+          "committer": {
+            "email": "edithwuly@163.com",
+            "name": "wlywly",
+            "username": "edithwuly"
+          },
+          "distinct": true,
+          "id": "4b2ebaaaf21b43192238149c83652783a3fe0978",
+          "message": "add e2e benchmark for --buildpack",
+          "timestamp": "2023-04-21T16:44:12+08:00",
+          "tree_id": "054cac51ff3d7cfe74c2c1bcb987c15f4fc53f6d",
+          "url": "https://github.com/edithwuly/pack/commit/4b2ebaaaf21b43192238149c83652783a3fe0978"
+        },
+        "date": 1682066812613,
         "tool": "go",
         "benches": [
           {
