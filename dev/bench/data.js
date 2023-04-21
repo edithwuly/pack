b4c2da45b94524f271b54f3585d35a1ca12569c8
github-action-benchmark <github@users.noreply.github.com>
2023-04-21 07:37:47 +0000

    add Go Benchmark (go) benchmark result for 8ac4cae66914fb4fcabc52ac6a8f2f671283eb09

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1682062667513,
+  "repoUrl": "https://github.com/edithwuly/pack",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "33151775+edithwuly@users.noreply.github.com",
+            "name": "edithwuly",
+            "username": "edithwuly"
+          },
+          "committer": {
+            "email": "edithwuly@163.com",
+            "name": "wlywly",
+            "username": "edithwuly"
+          },
+          "distinct": true,
+          "id": "8ac4cae66914fb4fcabc52ac6a8f2f671283eb09",
+          "message": "Create benchmark.yml\n\nSigned-off-by: edithwuly <33151775+edithwuly@users.noreply.github.com>",
+          "timestamp": "2023-04-21T15:35:21+08:00",
+          "tree_id": "bd9e3799a3ce359dae209b8a9d97ffb83c3926d3",
+          "url": "https://github.com/edithwuly/pack/commit/8ac4cae66914fb4fcabc52ac6a8f2f671283eb09"
+        },
+        "date": 1682062666720,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkBuild/with_Untrusted_Builder",
+            "value": 5697231274,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Trusted_Builder",
+            "value": 1602010265,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
